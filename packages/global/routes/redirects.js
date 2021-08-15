a904633bd19547c787d350c39490c4cdf3ce2d7e
Brandon Krigbaum <brandonbk@gmail.com>
2021-08-15 16:27:48 -0500

    Update about-us, contact-us redirects

--- packages/global/routes/redirects.js
+++ packages/global/routes/redirects.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
-  app.get('/:alias(contact-us|about-us|contact-our-staff)', (req, res) => {
+  app.get('/:alias(contact-us|contact-us.html)', (req, res) => {
     res.redirect(301, '/page/contact-us');
+  });
+  app.get('/:alias(about-us|about-us.html)', (req, res) => {
+    res.redirect(301, '/page/about-us');
   });
   app.get('/:alias(feed)', (req, res) => {
     res.redirect(301, '/__rss/website-scheduled-content.xml?input=%7B"sectionAlias"%3A"home"%7D');
