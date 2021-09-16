6d173f215536ef1b623768de05823f56e65b2961
Brian Miller <brian@parameter1.com>
2021-09-16 10:04:01 -0500

    Add Premium Partner Config to Aqua

---
+++ sites/aquamagazine.com/config/premium-partners.js
@@ -0,0 +1,88 @@
+module.exports = [
+  {
+    img: 'https://aquamagazine.com/images/banners/2021/AquaStar-Logo_229x100.jpg',
+    name: 'AquaStar Pool Products',
+    href: '#',
+  },
+  {
+    img: 'https://aquamagazine.com/images/banners/2017/1017/Pentair_102617.png',
+    name: 'Pentair',
+    href: '#',
+  },
+  {
+    img: 'https://aquamagazine.com/images/banners/2017/1017/RBControls_102617.png',
+    name: 'RB Control Systems',
+    href: '#',
+  },
+  {
+    img: 'https://aquamagazine.com/images/banners/2018/20180228/SpeckPumps_022818.png',
+    name: 'Speck Pumps',
+    href: '#',
+  },
+  {
+    img: 'https://aquamagazine.com/images/banners/2018/20180614/CMP_061418.png',
+    name: 'CMP',
+    href: '#',
+  },
+  {
+    img: 'https://aquamagazine.com/images/banners/2018/20180228/Intermatic_022818.png',
+    name: 'Intermatic',
+    href: '#',
+  },
+  // Unused right now
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2021/Skimmer_Logo_pdf_280x100.jpg',
+  //   name: 'Skimmer',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/0416/AQ_Prem_Pleato_042116.png',
+  //   name: 'Pure',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2017/0817/Coverlon_080817.png',
+  //   name: 'Coverlon',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2019/20191206/Fluidra_280x100.jpg',
+  //   name: 'zodiac',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/0416/AQ_Prem_Hayward_042116.png',
+  //   name: 'Hayward',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2021/tara-logo-280x100_2.jpg',
+  //   name: 'Tara Liners',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2020/LathamPools_Wordmark-Blue_280x100.jpg',
+  //   name: 'Latham',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2018/20181120/LanXess_Oxone_280x100.png',
+  //   name: 'LanXess',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2020/LyonFinancialLogo_280x100.jpg',
+  //   name: 'Lyon Financial',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2020/HFS_LOGO_FINAL_280x100.jpg',
+  //   name: 'HFS Financial',
+  //   href: '#',
+  // },
+  // {
+  //   img: 'https://aquamagazine.com/images/banners/2021/Viking-Capital-MASTER-LOGO_280x100.jpg',
+  //   name: 'Viking Capital',
+  //   href: '#',
+  // },
+];
