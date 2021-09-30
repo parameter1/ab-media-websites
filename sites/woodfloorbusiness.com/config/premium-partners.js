5f47a04f952b4355e23d40858124c8d364c486f1
Brandon Krigbaum <brandonbk@gmail.com>
2021-09-30 16:38:42 -0500

    Update premium partner image domains

--- sites/woodfloorbusiness.com/config/premium-partners.js
+++ sites/woodfloorbusiness.com/config/premium-partners.js
@@ -1,52 +1,52 @@
 module.exports = [
   {
     href: 'https://wfbmag.co/BonaPPLogo',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/Bona_300dpi.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/Bona_300dpi.png',
     name: 'bona',
   },
   {
     href: 'http://staufusa.com',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/Stauf.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/Stauf.png',
     name: 'Stauf',
   },
   {
     href: 'https://www.nortonabrasives.com/en-us/markets/floor-sanding?utm_source=Wood%20Floor%20Business',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/Norton_LOGO.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/Norton_LOGO.png',
     name: 'Norton',
   },
   {
     href: 'https://wfbmag.co/SaroyanHdwds',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/NEW2020logos.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/NEW2020logos.png',
     name: 'Saroyan Hardwoods',
   },
   {
     href: 'http://www.woodwise.com/',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/woodwiselogocolor.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/woodwiselogocolor.png',
     name: 'Woodwise',
   },
   {
     href: 'https://wfbmag.co/laglerlogo',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/Lagler_2x2_300dpi.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/Lagler_2x2_300dpi.png',
     name: 'Lagler',
   },
   {
     href: 'https://wfbmag.co/3kz2mVW',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/ABSCO-logo.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/ABSCO-logo.png',
     name: 'Absco',
   },
   {
     href: 'https://wfbmag.co/32b8kDY',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/HFP-Logo_color.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/HFP-Logo_color.png',
     name: 'Horizon Forest Products',
   },
   {
     href: 'https://wfbmag.co/3oWX33V',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/AmericanSandersLogo.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/AmericanSandersLogo.png',
     name: 'American Sanders',
   },
   {
     href: 'https://grafbro.com/flooring/solid/',
-    imagePath: 'https://img.aquamagazine.com/files/base/abmedia/all/image/static/wfb/premium-partners/GRAFBros_Logo_cmyk300.png',
+    imagePath: 'https://img.woodfloorbusiness.com/files/base/abmedia/all/image/static/wfb/premium-partners/GRAFBros_Logo_cmyk300.png',
     name: 'Graf Bros',
   },
 ];
