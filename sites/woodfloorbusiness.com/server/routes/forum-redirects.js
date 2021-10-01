6ba2bc858be68b4dc4e8ec95125d75a2c0003cd1
Josh Worden <josh@parameter1.com>
2021-10-01 10:29:28 -0500

    change structure

--- sites/woodfloorbusiness.com/server/routes/forum-redirects.js
+++ sites/woodfloorbusiness.com/server/routes/forum-redirects.js
@@ -3,11 +3,10 @@
  */
 module.exports = (app) => {
   const to = '/page/forum';
-  const urls = [
+  [
     '/forum',
     '/floormasters*',
     '/component/com_kunena/*',
     '/viewforum.php',
-  ];
-  urls.forEach(url => app.get(url, (_, res) => res.redirect(302, to)));
+  ].forEach(url => app.get(url, (_, res) => res.redirect(302, to)));
 };
