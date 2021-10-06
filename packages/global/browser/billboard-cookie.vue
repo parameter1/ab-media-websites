85e18fac1400e7df8c2bafdda264d8ac59b11486
Brandon Krigbaum <brandonbk@gmail.com>
2021-10-06 12:46:34 -0500

    Add billboard-cookie vue component

---
+++ packages/global/browser/billboard-cookie.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="billboard-cookie" style="display: none;" />
+</template>
+<script>
+import cookies from 'js-cookie';
+
+export default {
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+    cookieName: {
+      type: String,
+      required: true,
+    },
+    cookieValue: {
+      type: String,
+      default: 'true',
+    },
+    expires: {
+      type: Number,
+      default: 1,
+    },
+  },
+  created() {
+    const { googletag } = window;
+    googletag.cmd.push(() => {
+      googletag.pubads().addEventListener('slotOnload', (event) => {
+        if (event.slot.getAdUnitPath() === this.path) {
+          cookies.set(this.cookieName, this.cookieValue, { expires: this.expires });
+        }
+      });
+    });
+  },
+};
+</script>
