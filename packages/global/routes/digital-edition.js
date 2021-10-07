07c3568e9ee6f14d1c6bc93c222597b642cd01a6
Brandon Krigbaum <brandonbk@gmail.com>
2021-10-07 14:02:33 -0500

    Update redirect route regex

--- packages/global/routes/digital-edition.js
+++ packages/global/routes/digital-edition.js
@@ -2,7 +2,7 @@ const { getAsObject } = require('@parameter1/base-cms-object-path');
 const digitalEdition = require('../templates/digital-edition');
 
 module.exports = (app, siteConfig) => {
-  app.get('/images/digitalissues/:code([0-9]{4})', (req, res) => {
+  app.get('/images/digitalissues/:code([a-zA-Z0-9]{4})', (req, res) => {
     const { redirects } = getAsObject(siteConfig, 'digitalEdition');
     const zmagId = redirects[req.params.code];
     const to = `/digital-edition/${zmagId}`;
