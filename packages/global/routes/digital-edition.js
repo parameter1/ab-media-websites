7e0b6a01ad05298ab2f4bce58fe4f8c64429b344
Brandon Krigbaum <brandonbk@gmail.com>
2021-10-07 11:46:30 -0500

    Add redirect handler

--- packages/global/routes/digital-edition.js
+++ packages/global/routes/digital-edition.js
@@ -1,6 +1,14 @@
+const { getAsObject } = require('@parameter1/base-cms-object-path');
 const digitalEdition = require('../templates/digital-edition');
 
 module.exports = (app, siteConfig) => {
+  app.get('/images/digitalissues/:code([0-9]{4})', (req, res) => {
+    const { redirects } = getAsObject(siteConfig, 'digitalEdition');
+    const zmagId = redirects[req.params.code];
+    const to = `/digital-edition/${zmagId}`;
+    res.redirect(301, to);
+  });
+
   app.get('/digital-edition/:id([a-fA-F0-9]{8})', (req, res) => {
     res.marko(digitalEdition, { id: req.params.id });
   });
