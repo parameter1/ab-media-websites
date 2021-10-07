39140792b7323448d9f3c02636f0d28c484b6189
Brandon Krigbaum <brandonbk@gmail.com>
2021-10-07 14:50:48 -0500

    Update redirect handler

--- packages/global/routes/digital-edition.js
+++ packages/global/routes/digital-edition.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const { getAsObject } = require('@parameter1/base-cms-object-path');
 const digitalEdition = require('../templates/digital-edition');
 
@@ -5,8 +6,11 @@ module.exports = (app, siteConfig) => {
   app.get('/images/digitalissues/:code([a-zA-Z0-9]{4})', (req, res) => {
     const { redirects } = getAsObject(siteConfig, 'digitalEdition');
     const zmagId = redirects[req.params.code];
-    const to = `/digital-edition/${zmagId}`;
-    res.redirect(301, to);
+    if (zmagId) {
+      const to = `/digital-edition/${zmagId}/`;
+      res.redirect(301, to);
+    }
+    throw createError(404, `No digital edition found for '${req.params.code}'`);
   });
 
   app.get('/digital-edition/:id([a-fA-F0-9]{8})', (req, res) => {
