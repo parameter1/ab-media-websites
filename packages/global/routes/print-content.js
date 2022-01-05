69ffc906d48ec56740827f641593830b0dc4d692
Brandon Krigbaum <brandonbk@gmail.com>
2022-01-05 17:46:18 -0600

    Restore global content-page fragment

--- packages/global/routes/print-content.js
+++ packages/global/routes/print-content.js
@@ -1,5 +1,5 @@
 const { withContent } = require('@parameter1/base-cms-marko-web/middleware');
-const queryFragment = require('@ab-media/package-theme-monorail/graphql/fragments/content-page');
+const queryFragment = require('@ab-media/package-global/graphql/fragments/content-page');
 const print = require('../templates/content/print');
 
 module.exports = (app) => {
