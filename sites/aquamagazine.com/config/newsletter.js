122c9724a402707899049f7a5ca52abcb66cb256
Brian Miller <brian@parameter1.com>
2022-06-22 07:06:16 -0500

    Clean up newsletter config.
    @todo: revisit if this is the best way to configure these or if we should change the current newsletter config

--- sites/aquamagazine.com/config/newsletter.js
+++ sites/aquamagazine.com/config/newsletter.js
@@ -8,64 +8,6 @@ const baseConfig = {
 const defaults = {
   name: 'Your Industry Connection',
   description: 'Join other industry pros who get pertinent, fast-reading nationwide stories delivered twice a week to their inbox with AQUA Today.',
-  privacyPolicy: {
-    href: '/page/privacy-policy',
-    label: 'Terms of Use and Privacy Policy',
-  },
-  defaultNewsletter: {
-    deploymentTypeId: 11,
-    name: 'AQUA Today',
-    eventCategory: 'Daily AQUA Subscription',
-  },
-  newsletters: [
-    {
-      deploymentTypeId: 11,
-      name: 'AQUA Today',
-      description: 'Professionals within the spa and pool industry to provide pertinent, fast-reading nationwide stories.',
-      eventCategory: 'AQUA Product Subscription',
-    },
-    {
-      deploymentTypeId: 16,
-      name: 'AQUA Product Updates',
-      description: 'Leading products & services from the industry\'s top suppliers.',
-      eventCategory: 'AQUA Product Subscription',
-    },
-    {
-      deploymentTypeId: 15,
-      name: 'AQUA Editorial Promotions',
-      description: 'Important updates on AQUA Magazine, online content, digital issues and more.',
-      eventCategory: 'AQUA Editorial Subscription',
-    },
-    {
-      deploymentTypeId: 14,
-      name: 'AQUA Marketing Promotions',
-      description: 'Information on upcoming contests, surveys and more.',
-      eventCategory: 'AQUA Marketing Subscription',
-    },
-    {
-      deploymentTypeId: 30,
-      name: 'AQUA Live Promotional Emails',
-      description: 'AQUA Live conference and expo floor updates, and messages from our sponsors.',
-      eventCategory: 'AQUA Live Subscription',
-    },
-  ],
-  demographic: {
-    id: 7,
-    label: 'Your primary role?',
-    values: [
-      { id: 3, label: 'Pool Dealer/Builder Only' },
-      { id: 4, label: 'Pool and Spa Dealer/Builder' },
-      { id: 5, label: 'Spa Dealer Only' },
-      { id: 6, label: 'Pool and Spa Supply Dealer' },
-      { id: 7, label: 'Maintenance/Service' },
-      { id: 8, label: 'Distributor/Wholesaler' },
-      { id: 9, label: 'Manufacturer, Manufacturer\'s Rep.' },
-      { id: 10, label: 'Casual Furniture/Patio Dealer' },
-      { id: 11, label: 'Landscape Architect/Designer' },
-      { id: 12, label: 'Solar/Sauna Dealer' },
-      { id: 13, label: 'Other)' },
-    ],
-  },
 };
 
 module.exports = {
--- sites/woodfloorbusiness.com/config/newsletter.js
+++ sites/woodfloorbusiness.com/config/newsletter.js
@@ -8,62 +8,6 @@ const baseConfig = {
 const defaults = {
   name: 'Your Industry Connection',
   description: 'Join other industry professionals in the wood flooring industry to provide the latest news, information and insight delivered to your inbox twice a week.',
-  privacyPolicy: {
-    href: '/page/privacy-policy',
-    label: 'Terms of Use and Privacy Policy',
-  },
-  defaultNewsletter: {
-    deploymentTypeId: 23,
-    name: 'WFB E-News',
-    eventCategory: 'WFB E-News Subscription',
-  },
-  newsletters: [
-    {
-      deploymentTypeId: 23,
-      name: 'WFB E-News',
-      description: 'Sent Mondays and Wednesdays to professionals in the wood flooring industry to provide the latest news, information and insight',
-      eventCategory: 'WFB E-News Subscription',
-    },
-    {
-      deploymentTypeId: 27,
-      name: 'WFB Product Updates',
-      description: 'Leading products & special offers from the industry’s top suppliers.',
-      eventCategory: 'WFB Product Subscription',
-    },
-    {
-      deploymentTypeId: 26,
-      name: 'WFB Editorial Promotions',
-      description: 'Important updates on Wood Floor Business, online content, digital issues, content roundups and more.',
-      eventCategory: 'WFB Editorial Subscription',
-    },
-    {
-      deploymentTypeId: 25,
-      name: 'WFB Marketing Promotions',
-      description: 'Information on upcoming contests, surveys and more.',
-      eventCategory: 'WFB Marketing Subscription',
-    },
-    {
-      deploymentTypeId: 28,
-      name: 'WFB Contractor Panel',
-      description: 'These surveys will give contractors an opportunity to share their opinions about products and more.',
-      eventCategory: 'WFB Contractor Panel Subscription',
-    },
-  ],
-  demographic: {
-    id: 11,
-    label: 'Your primary role?',
-    values: [
-      { id: 118, label: 'Installer/Contractor/Sander/Refinisher' },
-      { id: 119, label: 'Retail Floor Covering Store' },
-      { id: 120, label: 'Distributor/Manufacturer Representative' },
-      { id: 121, label: 'Manufacturer/Importer/Exporter' },
-      { id: 122, label: 'Architect' },
-      { id: 123, label: 'Interior Designer' },
-      { id: 124, label: 'Builder' },
-      { id: 125, label: 'Inspector' },
-      { id: 126, label: 'Other' },
-    ],
-  },
 };
 
 module.exports = {
