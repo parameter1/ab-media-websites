693ff16aefa9ff926976e9cf5125638ddd767ebb
Brian Miller <brian@parameter1.com>
2022-07-13 13:27:55 -0500

    update newsletter config image paths

--- sites/woodfloorbusiness.com/config/newsletter.js
+++ sites/woodfloorbusiness.com/config/newsletter.js
@@ -13,18 +13,18 @@ const defaults = {
 module.exports = {
   pushdown: {
     ...defaults,
-    imagePath: 'files/base/p1/sandbox/image/static/newsletter-phone-half.png',
+    imagePath: 'files/base/abmedia/all/image/static/newsletter-signup/wfb-signup-half.png',
     description: 'Join other industry professionals in the wood flooring industry to provide the latest news, information and insight delivered to your inbox twice a week.',
   },
   signupBanner: {
     ...defaults,
-    imagePath: 'files/base/p1/sandbox/image/static/newsletter-phone-full.png',
+    imagePath: 'files/base/abmedia/all/image/static/newsletter-signup/wfb-signup-full.png',
   },
   signupBannerLarge: {
     ...defaults,
     name: 'Newsletter for Wood Floor Professionals',
     description: 'Get industry news, insights, and new product information delivered straight to your inbox.',
-    imagePath: 'files/base/p1/sandbox/image/static/newsletter-phone-full.png',
+    imagePath: 'files/base/abmedia/all/image/static/newsletter-signup/wfb-signup-full.png',
   },
   signupFooter: {
     ...baseConfig,
